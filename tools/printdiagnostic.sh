a0ea61cdaed46e08183b248936a0261d8e2c9c8b
Jun Sun <jusu@microsoft.com>
2019-11-01 10:47:27 -0700

    Simplify print diagnostic script to just output JSON nice format.

--- tools/printdiagnostic.sh
+++ tools/printdiagnostic.sh
@@ -2,19 +2,15 @@
 
 echo
 echo 1. Network Setup
-echo
 for NODEAPD in $(kubectl -n aks-periscope get apd -o name)
 do
-    kubectl -n aks-periscope get $NODEAPD -o jsonpath="{.spec.networkconfig}" | jq  -r '["HostName", "NetworkPlugin", "VirtualMachineDNS", "KubernetesDNS", "MaxPodsPerNode"] as $fields | $fields, (. | [(.[$fields[]]|@json)]) | @tsv' | column -t
-    echo
+    kubectl -n aks-periscope get $NODEAPD -o jsonpath="{.spec.networkconfig}" | jq .
 done
-echo
+
 echo
 echo 2. Network Outbound Check
-echo
 for NODEAPD in $(kubectl -n aks-periscope get apd -o name)
 do
-    kubectl -n aks-periscope get $NODEAPD -o jsonpath="{.spec.networkoutbound}" | jq  -r '["HostName", "Type", "Start", "End", "Status"] as $fields | $fields, (.[] | [(.[$fields[]]|@json)]) | @tsv' | column -t
+    kubectl -n aks-periscope get $NODEAPD -o jsonpath="{.spec.networkoutbound}" | jq .
     echo
 done
-echo
