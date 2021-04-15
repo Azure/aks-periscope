afc20c4350cc71005c73d87246a194145e4edb9b
Sanya Kochhar <kochhars@microsoft.com>
2021-04-15 13:37:03 -0400

    add allresources/config files
    Signed-off-by: Sanya Kochhar <kochhars@microsoft.com>

--- pkg/collector/osmlogs_collector.go
+++ pkg/collector/osmlogs_collector.go
@@ -30,11 +30,12 @@ func (collector *OsmLogsCollector) Collect() error {
 	if err != nil {
 		return err
 	}
-	// will want multiple files here - can define resources to query in deployment.yaml and iterate through the commands+resources needed and create multiple files
+	// can define resources to query in deployment.yaml and iterate through the commands+resources needed and create multiple files
 	// see kubeobject collector for example
-	allResourcesFile := filepath.Join(rootPath, "allResources")
 
-	output, err := utils.RunCommandOnContainer("kubectl", "get", "all", "--all-namespaces", "--selector", "app.kubernetes.io/name=openservicemesh.io", "-o", "json")
+	// Get osm resources as table
+	allResourcesFile := filepath.Join(rootPath, "allResources")
+	output, err := utils.RunCommandOnContainer("kubectl", "get", "all", "--all-namespaces", "--selector", "app.kubernetes.io/name=openservicemesh.io")
 	if err != nil {
 		return err
 	}
@@ -46,5 +47,28 @@ func (collector *OsmLogsCollector) Collect() error {
 
 	collector.AddToCollectorFiles(allResourcesFile)
 
+	// Get osm resource configs
+	allResourceConfigsFile := filepath.Join(rootPath, "allResourceConfigs")
+	output, err = utils.RunCommandOnContainer("kubectl", "get", "all", "--all-namespaces", "--selector", "app.kubernetes.io/name=openservicemesh.io", "-o", "json")
+	if err != nil {
+		return err
+	}
+
+	err = utils.WriteToFile(allResourceConfigsFile, output)
+	if err != nil {
+		return err
+	}
+
+	collector.AddToCollectorFiles(allResourceConfigsFile)
+
 	return nil
+}
+
+// Helper function to get all meshes in the cluster
+func getMeshList() (string, error) {
+	meshList, err := utils.RunCommandOnContainer("kubectl", "get", "deployments", "--all-namespaces", "-o=jsonpath=\"{..meshName}\"", "-l", "app=osm-controller")
+	if err != nil {
+		return "", err
+	}
+	return meshList, nil
 }
