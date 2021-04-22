567c7de4233235c9bd8970f3754ef1ce7ad40b41
Sophie Zhao <sophiezhao@microsoft.com>
2021-04-22 17:53:52 -0400

    add helm collector

--- pkg/collector/helm_collector.go
+++ pkg/collector/helm_collector.go
@@ -1,7 +1,10 @@
 package collector
 
 import (
+	"path/filepath"
+
 	"github.com/Azure/aks-periscope/pkg/interfaces"
+	"github.com/Azure/aks-periscope/pkg/utils"
 )
 
 // HelmCollector defines a Helm Collector struct
@@ -23,5 +26,32 @@ func NewHelmCollector(exporter interfaces.Exporter) *HelmCollector {
 
 // Collect implements the interface method
 func (collector *HelmCollector) Collect() error {
+	rootPath, err := utils.CreateCollectorDir(collector.GetName())
+	if err != nil {
+		return err
+	}
+	helmListFile := filepath.Join(rootPath, collector.GetName())
+	output, err := utils.RunCommandOnHost("helm", "list", "--all-namespaces")
+	if err != nil {
+		return err
+	}
+	err = utils.WriteToFile(helmListFile, output)
+	if err != nil {
+		return err
+	}
+
+	collector.AddToCollectorFiles(helmListFile)
+
+	helmHistoryFile := filepath.Join(rootPath, collector.GetName())
+	output, err = utils.RunCommandOnHost("helm", "history", "azure-arc")
+	if err != nil {
+		return err
+	}
+	err = utils.WriteToFile(helmHistoryFile, output)
+	if err != nil {
+		return err
+	}
+
+	collector.AddToCollectorFiles(helmHistoryFile)
 	return nil
 }
