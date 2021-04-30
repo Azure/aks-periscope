1a62ca5ae16b0f4a9cdda3e5336e9d2cb1f736eb
Sophie Zhao <sophiezhao@microsoft.com>
2021-04-30 14:47:53 -0400

    test zip

--- pkg/exporter/localmachine_exporter.go
+++ pkg/exporter/localmachine_exporter.go
@@ -2,7 +2,7 @@ package exporter
 
 import (
 	"archive/zip"
-	"io"
+	"fmt"
 	"log"
 	"os"
 
@@ -20,66 +20,47 @@ var _ interfaces.Exporter = &LocalMachineExporter{}
 
 // Export implements the interface method
 func (exporter *LocalMachineExporter) Export(files []string) error {
-	output := "done.zip"
-
-	if err := ZipFiles(output, files); err != nil {
-		panic(err)
-	}
-	log.Printf("Zipped File: %s", output)
-	return nil
-}
-func ZipFiles(filename string, files []string) error {
-
-	newZipFile, err := os.Create(filename)
+	outFile, err := os.Create("test.zip")
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	defer newZipFile.Close()
+	defer outFile.Close()
 
-	zipWriter := zip.NewWriter(newZipFile)
-	defer zipWriter.Close()
+	// Create a zip writer on top of the file writer
+	zipWriter := zip.NewWriter(outFile)
 
-	// Add files to zip
-	for _, file := range files {
-		log.Printf("Filename: %s", file)
-		if err = AddFileToZip(zipWriter, file); err != nil {
-			return err
+	// Add files to archive
+	// We use some hard coded data to demonstrate,
+	// but you could iterate through all the files
+	// in a directory and pass the name and contents
+	// of each file, or you can take data from your
+	// program and write it write in to the archive
+	// without
+	var filesToArchive = []struct {
+		Name, Body string
+	}{
+		{"test.txt", "String contents of file"},
+		{"test2.txt", "\x61\x62\x63\n"},
+	}
+	fmt.Printf("I am here")
+	// Create and write files to the archive, which in turn
+	// are getting written to the underlying writer to the
+	// .zip file we created at the beginning
+	for _, file := range filesToArchive {
+		fileWriter, err := zipWriter.Create(file.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = fileWriter.Write([]byte(file.Body))
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
+
+	// Clean up
+	err = zipWriter.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 	return nil
-}
-
-func AddFileToZip(zipWriter *zip.Writer, filename string) error {
-
-	fileToZip, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer fileToZip.Close()
-
-	// Get the file information
-	info, err := fileToZip.Stat()
-	if err != nil {
-		return err
-	}
-
-	header, err := zip.FileInfoHeader(info)
-	if err != nil {
-		return err
-	}
-
-	// Using FileInfoHeader() above only uses the basename of the file. If we want
-	// to preserve the folder structure we can overwrite this with the full path.
-	header.Name = filename
-
-	// Change to deflate to gain better compression
-	// see http://golang.org/pkg/archive/zip/#pkg-constants
-	header.Method = zip.Deflate
-
-	writer, err := zipWriter.CreateHeader(header)
-	if err != nil {
-		return err
-	}
-	_, err = io.Copy(writer, fileToZip)
-	return err
 }
