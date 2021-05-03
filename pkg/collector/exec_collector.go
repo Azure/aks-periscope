28a0fb2a7ea5815916a12b0de70c496408837fa6
Sophie Zhao <sophiezhao@microsoft.com>
2021-05-03 12:09:12 -0400

    add exec collector

--- pkg/collector/exec_collector.go
+++ pkg/collector/exec_collector.go
@@ -1,7 +1,12 @@
 package collector
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	"github.com/Azure/aks-periscope/pkg/interfaces"
+	"github.com/Azure/aks-periscope/pkg/utils"
 )
 
 // ExecCollector defines a Exec Collector struct
@@ -23,5 +28,31 @@ func NewExecCollector(exporter interfaces.Exporter) *ExecCollector {
 
 // Collect implements the interface method
 func (collector *ExecCollector) Collect() error {
+	rootPath, err := utils.CreateCollectorDir(collector.GetName())
+	if err != nil {
+		return err
+	}
+	namespaces := strings.Fields(os.Getenv("DIAGNOSTIC_EXEC_LIST"))
+	for _, namespace := range namespaces {
+		output, err := utils.RunCommandOnContainer("kubectl", "get", "-n", namespace, "pods", "--output=jsonpath={.items..metadata.name}")
+		if err != nil {
+			return err
+		}
+		pods := strings.Split(output, " ")
+
+		for _, pod := range pods {
+			execLog := filepath.Join(rootPath, namespace+"_"+pod)
+			output, err := utils.RunCommandOnContainer("kubectl", "-n", namespace, "exec", pod, "--", "curl", "example.com")
+			if err != nil {
+				return err
+			}
+			err = utils.WriteToFile(execLog, output)
+			if err != nil {
+				return err
+			}
+
+			collector.AddToCollectorFiles(execLog)
+		}
+	}
 	return nil
 }
