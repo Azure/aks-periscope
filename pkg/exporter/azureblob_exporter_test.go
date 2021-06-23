92a767ce26a0df2dea32cd58b4c35c35abeb6e7c
david kydd <david.kydd@microsoft.com>
2021-06-24 11:11:22 +1200

    additional test cases

--- pkg/exporter/azureblob_exporter_test.go
+++ pkg/exporter/azureblob_exporter_test.go
@@ -10,6 +10,8 @@ var getStorageContainerNameTests = []struct {
 }{
 	{"standard-aks-fqdn-dns-d0daedb9.hcp.eastus.azmk8s.io", "standard-aks-fqdn-dns-d0daedb9"},
 	{"aks-engine-fqdn.westeurope.cloudapp.azure.com", "aks-engine-fqdn-westeurope-cloudapp-azure-com"},
+	{"additional.aks-engine-fqdn.db839748.eastus.cloudapp.azure.com", "additional-aks-engine-fqdn-db839748-eastus-cloudapp-azure-com"},
+	{"10.255.255.5", "10-255-255-5"}, // aks-engine clusters will currently return an IPv4 address as what Periscope is calling the APIServerFQDN
 	{"extra.super.duper.long.apiserverfqdn.that.has.more.than.63.characters", "extra-super-duper-long-apiserverfqdn-that-has-more-than-63-char"},
 }
 
