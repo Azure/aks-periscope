77dd262ab976b9463ca85037e27ed08ef1f70487
david kydd <david.kydd@microsoft.com>
2021-07-02 16:58:53 +1200

    fix containerd logs tests

--- pkg/collector/containerlogs_collector_containerd_test.go
+++ pkg/collector/containerlogs_collector_containerd_test.go
@@ -24,12 +24,11 @@ func TestParseContainerLogSelectors(t *testing.T) {
 	for _, tt := range parseContainerLogSelectorTests {
 		testName := strings.Join(tt.selectorString, " ")
 		t.Run(testName, func(t *testing.T) {
-			var collector = &ContainerLogsCollectorContainerD{}
-			selectors := collector.ParseContainerLogSelectors(tt.selectorString)
+			selectors := parseContainerLogSelectors(tt.selectorString)
 
 			for i, selector := range selectors {
 				if selector != tt.containerLogSelector[i] {
-					t.Errorf("Sprintf(%q, &collector) => %q, want %q",
+					t.Errorf("parseContainerLogSelectors(%q) => %q, want %q",
 						testName, selector, tt.containerLogSelector[i])
 				}
 			}
@@ -48,7 +47,7 @@ var ParseContainerLogFilenamesTests = []struct {
 			namespace:     "kube-system",
 			containerName: "kube-apiserver",
 			containeruid:  "341694b9f16b51d7afc7c0a68d2ea44f31f6c2dad550d56d8d8dd9304a27b01f",
-			filepath:      "/var/logs/containers/kube-apiserver-kind-control-plane_kube-system_kube-apiserver-341694b9f16b51d7afc7c0a68d2ea44f31f6c2dad550d56d8d8dd9304a27b01f.log"}}},
+			filepath:      "/var/log/containers/kube-apiserver-kind-control-plane_kube-system_kube-apiserver-341694b9f16b51d7afc7c0a68d2ea44f31f6c2dad550d56d8d8dd9304a27b01f.log"}}},
 	{containerLogDirectory, []string{
 		"metrics-agent-5b9b94754f-lv4gm_azure-arc_metrics-agent-b2aa15b3f9c7c395539281bd94ee0725706c7228439a8870494804e042a54d7d.log",
 		"resource-sync-agent-f8c7c6b6b-zdqkc_azure-arc_fluent-bit-46d805fc1d05986cce94f845401949c241fa577e4312df45e5bfb39b27dc226c.log"},
@@ -58,27 +57,26 @@ var ParseContainerLogFilenamesTests = []struct {
 			namespace:     "azure-arc",
 			containerName: "metrics-agent",
 			containeruid:  "b2aa15b3f9c7c395539281bd94ee0725706c7228439a8870494804e042a54d7d",
-			filepath:      "/var/logs/containers/kube-apiserver-kind-control-plane_kube-system_kube-apiserver-341694b9f16b51d7afc7c0a68d2ea44f31f6c2dad550d56d8d8dd9304a27b01f.log"}, {
+			filepath:      "/var/log/containers/metrics-agent-5b9b94754f-lv4gm_azure-arc_metrics-agent-b2aa15b3f9c7c395539281bd94ee0725706c7228439a8870494804e042a54d7d.log"}, {
 
 			podname:       "resource-sync-agent-f8c7c6b6b-zdqkc",
 			namespace:     "azure-arc",
 			containerName: "fluent-bit",
 			containeruid:  "46d805fc1d05986cce94f845401949c241fa577e4312df45e5bfb39b27dc226c",
-			filepath:      "/var/logs/containers/kube-apiserver-kind-control-plane_kube-system_kube-apiserver-341694b9f16b51d7afc7c0a68d2ea44f31f6c2dad550d56d8d8dd9304a27b01f.log"}}},
+			filepath:      "/var/log/containers/resource-sync-agent-f8c7c6b6b-zdqkc_azure-arc_fluent-bit-46d805fc1d05986cce94f845401949c241fa577e4312df45e5bfb39b27dc226c.log"}}},
 }
 
 // TestParseContainerLogSelectors test container log selector parser
 func TestParseContainerLogFilenames(t *testing.T) {
-	for _, tt := range parseContainerLogSelectorTests {
-		testName := strings.Join(tt.selectorString, " ")
+	for _, tt := range ParseContainerLogFilenamesTests {
+		testName := strings.Join(tt.containerLogFilenames, " ")
 		t.Run(testName, func(t *testing.T) {
-			var collector = &ContainerLogsCollectorContainerD{}
-			selectors := collector.ParseContainerLogSelectors(tt.selectorString)
+			containerLogs := parseContainerLogFilenames(containerLogDirectory, tt.containerLogFilenames)
 
-			for i, selector := range selectors {
-				if selector != tt.containerLogSelector[i] {
-					t.Errorf("Sprintf(%q, &collector) => %q, want %q",
-						testName, selector, tt.containerLogSelector[i])
+			for i, containerLog := range containerLogs {
+				if containerLog.filepath != tt.containerLogs[i].filepath {
+					t.Errorf("parseContainerLogFilenames(%q)=> %q, want %q",
+						testName, containerLog.filepath, tt.containerLogs[i].filepath)
 				}
 			}
 		})
