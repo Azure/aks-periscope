ac0f0626ec1c735e74751f91daa7f47d5b700b9a
david kydd <david.kydd@microsoft.com>
2021-07-02 16:21:37 +1200

    exportReader needs to be callable via exporter interface

--- pkg/interfaces/exporter.go
+++ pkg/interfaces/exporter.go
@@ -1,6 +1,9 @@
 package interfaces
 
+import "io"
+
 // Exporter defines interface for an exporter
 type Exporter interface {
 	Export(DataProducer) error
+	ExportReader(string, io.ReadSeeker) error
 }
