4be506bf9c6617095cc2a03f6ddf98f51ac04f89
Johnson Shi <Johnson.Shi@microsoft.com>
2021-07-12 16:47:21 -0700

    Create helper test file
    Signed-off-by: Johnson Shi <Johnson.Shi@microsoft.com>

--- tests/aksperiscopeintegration_test.go
+++ tests/aksperiscopeintegration_test.go
@@ -76,14 +76,3 @@ func checkifpodsrunning(t *testing.T) bool {
 
 	return false
 }
-
-func TestFilterSliceElemsWithTest(t *testing.T) {
-	g := NewGomegaWithT(t)
-
-	crdNameContainsSmiTestPredicate := func(s string) bool { return strings.Contains(s, "smi-spec.io") }
-	testSlice := []string{"abc", "httproutegroups.specs.smi-spec.io", "def", "tcproutes.specs.smi-spec.io", "ghi"}
-	expectedSlice := []string{"httproutegroups.specs.smi-spec.io", "tcproutes.specs.smi-spec.io"}
-	actualSlice := utils.FilterSliceElemsWithTestPredicate(testSlice, crdNameContainsSmiTestPredicate)
-
-	g.Expect(actualSlice).To(Equal(expectedSlice))
-}
