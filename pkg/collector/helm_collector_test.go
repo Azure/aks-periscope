a27f124883f1b631df2acaf37d234362de42392c
Arnaud <25585350+arnaud-tincelin@users.noreply.github.com>
2021-08-07 07:16:58 +0200

    Updated helm collector to leverage helm client for go (#90)
    Updated helm collector to leverage helm client for go, for arc scenarios.

---
+++ pkg/collector/helm_collector_test.go
@@ -0,0 +1,58 @@
+package collector
+
+import (
+	"encoding/json"
+	"os"
+	"path"
+	"testing"
+
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func TestHelmCollector(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "get release history",
+			want:    1,
+			wantErr: false,
+		},
+	}
+
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		t.Fatalf("Cannot get user home dir: %v", err)
+	}
+
+	master := ""
+	kubeconfig := path.Join(dirname, ".kube/config")
+	config, err := clientcmd.BuildConfigFromFlags(master, kubeconfig)
+	if err != nil {
+		t.Fatalf("Cannot load kube config: %v", err)
+	}
+
+	c := NewHelmCollector(config)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := c.Collect()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Collect() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			raw := c.GetData()["helm_list"]
+			var releases []HelmRelease
+
+			if err := json.Unmarshal([]byte(raw), &releases); err != nil {
+				t.Errorf("unmarshal GetData(): %v", err)
+			}
+
+			if len(releases) < tt.want {
+				t.Errorf("len(GetData()) = %v, want %v", len(releases), tt.want)
+			}
+		})
+	}
+}
