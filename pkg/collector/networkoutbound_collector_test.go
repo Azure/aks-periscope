ff0dafccfcdf836b45257fa2f72558c4884c9b90
Tatsinnit <tamishra@microsoft.com>
2021-09-06 10:15:11 +1200

    Unit-test for network-bound collectors.

---
+++ pkg/collector/networkoutbound_collector_test.go
@@ -0,0 +1,36 @@
+package collector
+
+import (
+	"testing"
+)
+
+func TestNewNetworkOutboundCollector(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    int
+		wantErr bool
+	}{
+		{
+			name:    "get networkbound logs",
+			want:    1,
+			wantErr: false,
+		},
+	}
+
+	c := NewNetworkOutboundCollector()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := c.Collect()
+
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Collect() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			raw := c.GetData()
+
+			if len(raw) < tt.want {
+				t.Errorf("len(GetData()) = %v, want %v", len(raw), tt.want)
+			}
+		})
+	}
+}
