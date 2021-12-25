0cefb4f243bf79f8da8a56fe5553d5832ce786ef
Tatsinnit <tamishra@microsoft.com>
2021-12-25 23:52:17 +1300

    kubeletcmd test structure.

---
+++ pkg/collector/kubeletcmd_collector_test.go
@@ -0,0 +1,38 @@
+package collector
+
+import (
+	"testing"
+)
+
+func TestNewKubeletcmdCollector(t *testing.T) {
+	tests := []struct {
+		name          string
+		want          int
+		wantErr       bool
+		collectorName string
+	}{
+		{
+			name:          "get kubeletcmd logs",
+			want:          1,
+			wantErr:       false,
+			collectorName: "kubeletcmd",
+		},
+	}
+
+	c := NewKubeletCmdCollector()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := c.Collect()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Collect() error = %v, wantErr %v", err, tt.wantErr)
+			}
+
+			// Get Data test needs to be written as the current test dont run at node level.
+			name := c.GetName()
+			if name != tt.collectorName {
+				t.Errorf("GetName()) = %v, want %v", name, tt.collectorName)
+			}
+		})
+	}
+}
