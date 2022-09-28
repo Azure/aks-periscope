b03c937a44f0fe4c5f05f98f842cfc6fd862a88b
Beilei Huang <41132767+bravebeaver@users.noreply.github.com>
2022-09-28 16:24:19 +1300

    allow periscope to run exec on pods

--- pkg/collector/inspecktor_trace_dns_collector.go
+++ pkg/collector/inspecktor_trace_dns_collector.go
@@ -1,8 +1,10 @@
 package collector
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"log"
 	"math/rand"
 	"strings"
 	"time"
@@ -10,9 +12,13 @@ import (
 	"github.com/Azure/aks-periscope/pkg/interfaces"
 	"github.com/Azure/aks-periscope/pkg/utils"
 	gadgetv1alpha1 "github.com/kinvolk/inspektor-gadget/pkg/apis/gadget/v1alpha1"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
 	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -95,12 +101,12 @@ func (collector *InspektorGadgetDNSTraceCollector) Collect() error {
 	if err != nil {
 		return fmt.Errorf("could not create dns trace %s: %w", gadgetName, err)
 	}
+
 	//collect output
-	output, err := utils.RunCommandOnHost("./bin/gadgettracermanager", "-call", "receive-stream", "-tracerid", fmt.Sprintf("trace_%s_%s", "gadget", gadgetName))
+	err = collector.runTraceCommandOnPod(gadgetName)
 	if err != nil {
 		return err
 	}
-	collector.data[fmt.Sprintf("dns-trace-%s", gadgetName)] = output
 
 	return nil
 }
@@ -116,4 +122,62 @@ func randomPodID() string {
 		output[i] = allowedCharacters[rand.Int31n(int32(len(allowedCharacters)))]
 	}
 	return string(output)
+}
+
+func (collector *InspektorGadgetDNSTraceCollector) runTraceCommandOnPod(gadgetName string) error {
+	// Creates the clientset
+	clientset, err := kubernetes.NewForConfig(collector.kubeconfig)
+	if err != nil {
+		return fmt.Errorf("getting access to K8S failed: %w", err)
+	}
+
+	gadgetPods, err := clientset.CoreV1().Pods("gadget").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return fmt.Errorf("could not list gadget pods: %w", err)
+	}
+	var command = []string{"./bin/gadgettracermanager", "-call", "receive-stream", "-tracerid", fmt.Sprintf("trace_gadget_%s", gadgetName)}
+
+	for _, podName := range gadgetPods.Items {
+		request := clientset.CoreV1().RESTClient().Post().
+			Resource("pods").
+			Name(podName.Name).
+			Namespace("gadget").
+			SubResource("exec").
+			VersionedParams(&v1.PodExecOptions{
+				Stdin:   false,
+				Stdout:  true,
+				Stderr:  true,
+				TTY:     false,
+				Command: command,
+			}, scheme.ParameterCodec)
+
+		stdout := new(bytes.Buffer)
+		stderr := new(bytes.Buffer)
+		streamOptions := remotecommand.StreamOptions{
+			Stdout: stdout,
+			Stderr: stderr,
+		}
+
+		exec, err := remotecommand.NewSPDYExecutor(collector.kubeconfig, "POST", request.URL())
+		log.Printf("\tPost request to DNS trace stream : %s ", request.URL())
+		if err != nil {
+			return fmt.Errorf("could not create SPDY executor: %w", err)
+		}
+
+		err = exec.Stream(streamOptions)
+		if err != nil {
+			result := strings.TrimSpace(stdout.String()) + "\n" + strings.TrimSpace(stderr.String())
+			result = strings.TrimSpace(result)
+			collector.data[fmt.Sprintf("dns-trace-%s-%s", gadgetName, podName)] = result
+			return fmt.Errorf("error reading stream: %w", err)
+		}
+		result := strings.TrimSpace(stdout.String()) + "\n" + strings.TrimSpace(stderr.String())
+		result = strings.TrimSpace(result)
+		log.Printf("\tObtain DNS trace stream : %s ", result)
+
+		collector.data[fmt.Sprintf("dns-trace-%s-%s", gadgetName, podName)] = result
+		log.Printf("\tupdated collector data")
+	}
+
+	return nil
 }
