f7e18fef82d3846291007215bbc9de699dd46a5b
Peter Bomber <peter.bomber@microsoft.com>
2023-05-16 13:59:01 +1200

    fix windows build errors

--- cmd/aks-periscope/collectorsfactory_windows.go
+++ cmd/aks-periscope/collectorsfactory_windows.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/Azure/aks-periscope/pkg/interfaces"
+	"github.com/Azure/aks-periscope/pkg/utils"
+	restclient "k8s.io/client-go/rest"
 )
 
 func addOSSpecificCollectors(collectors []interfaces.Collector, config *restclient.Config, runtimeInfo *utils.RuntimeInfo) []interfaces.Collector {
